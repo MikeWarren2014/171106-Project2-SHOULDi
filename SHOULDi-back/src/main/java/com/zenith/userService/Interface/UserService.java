c30e56207033338c14db2f9dd02334e29b4899c9
Caleb Schumake <cschumake@my.madonna.edu>
2017-12-19 23:00:18 -0500

    Added UserService interface and HibernateUtils class to make hibernate use easier

---
+++ SHOULDi-back/src/main/java/com/zenith/userService/Interface/UserService.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.zenith.userService.Interface;
+
+import com.zenith.beans.UserBean;
+
+/**
+ *
+ * @author calebschumake
+ */
+public interface UserService {
+    
+    public UserBean createUser(UserBean user); 
+    
+}
