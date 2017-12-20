b711a44648b932b1457ee9911610225b94c73d2c
Caleb Schumake <cschumake@my.madonna.edu>
2017-12-19 22:41:44 -0500

    Added all basic beans

---
+++ SHOULDi-back/src/main/java/com/zenith/beans/MessageBean.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.zenith.beans;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
+
+public class MessageBean {
+	@Id //designates as primary key
+	@Column(name="MESSAGE_ID")
+	@SequenceGenerator(sequenceName="MESSAGE_SEQ", name="MESSAGE_SEQ")
+	@GeneratedValue(generator="MESSAGE_SEQ", strategy=GenerationType.SEQUENCE)
+	private int message_id;
+	@OneToOne
+	@JoinColumn(name="USER_ID")
+	private int to;
+	@OneToOne
+	@JoinColumn( name="USER_ID")
+	private int from;
+	@Column(name="CONTENT")
+	private String content;
+	
+	
+	public int getMessage_id() {
+		return message_id;
+	}
+	public void setMessage_id(int message_id) {
+		this.message_id = message_id;
+	}
+	public int getTo() {
+		return to;
+	}
+	public void setTo(int to) {
+		this.to = to;
+	}
+	public int getFrom() {
+		return from;
+	}
+	public void setFrom(int from) {
+		this.from = from;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public MessageBean(int message_id, int to, int from, String content) {
+		super();
+		this.message_id = message_id;
+		this.to = to;
+		this.from = from;
+		this.content = content;
+	}
+	public MessageBean(int to, int from, String content) {
+		super();
+		this.to = to;
+		this.from = from;
+		this.content = content;
+	}
+	public MessageBean() {
+		super();
+	}
+	
+}
