29c4be0428c1716a3fa0bc53b6525c6e9fd319ca
Michael Warren <mwarren04011990@gmail.com>
2017-12-20 03:29:38 -0500

    modified .gitignore, worked on routing.ts,register.component.html,register.component.ts,app.component.html

---
+++ SHOULDi-front/src/app/components/register/register.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    // do we need module id here?
+    templateUrl : 'register.component.html',
+    selector    : 'register'
+})
+
+export class RegisterComponent
+{
+    
+    // TODO: implement this
+}
---
+++ SHOULDi-front/src/app/routing.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+
+import { RegisterComponent } from './components/register/register.component';
+
+export const appRoutes : Routes = [
+    {
+        path     : '/register',
+        component: RegisterComponent
+    }
+]
