721589f5f9e61a61b3423d0335ed9de71242d256
Michael Warren <mwarren04011990@gmail.com>
2017-12-22 13:34:12 -0500

    moved email field into general form part

--- SHOULDi-front/src/app/routing.ts
+++ SHOULDi-front/src/app/routing.ts
@@ -11,7 +11,7 @@ export const appRoutes : Routes = [
         children : [
             {
                 path       : '',
-                // redirectTo : '',
+                redirectTo : 'feed',
                 pathMatch  : 'full'
             },
             {
