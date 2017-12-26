88a24bf81789fb4bd2ef54554ef0508edc287d9d
caleb <waynecounty15@gmail.com>
2017-12-26 13:28:00 -0500

    most up to date

--- shouldi-back-current/src/main/java/com/zenith/DAO/PostDAO.java
+++ shouldi-back-current/src/main/java/com/zenith/DAO/PostDAO.java
@@ -1,5 +1,7 @@
 package com.zenith.DAO;
 
+import ImageUtils.ImageConversionUtil;
+import com.zenith.Beans.AdvertisementBean;
 import java.sql.Date;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -7,16 +9,25 @@ import java.util.List;
 import java.util.Random;
 
 import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.criterion.Restrictions;
+
+import com.zenith.Beans.CommentBean;
 import com.zenith.Beans.DislikeBean;
 import com.zenith.Beans.LikeBean;
 import com.zenith.Beans.PostBean;
 import com.zenith.Beans.UserBean;
 import com.zenith.Beans.VPBean;
 import com.zenith.hibernate.utils.HibernateUtils;
+import com.zenith.request.model.AdPostModel;
+import com.zenith.request.model.FlagPostModel;
 import com.zenith.request.model.PostModel;
+import com.zenith.request.model.RatingModel;
+
+import java.sql.Blob;
 
 public class PostDAO {
 
@@ -36,7 +47,6 @@ public class PostDAO {
     public List<PostBean> getFlaggedPosts() {
 
         session.beginTransaction();
-        Criteria criteria;
 
         List<PostBean> flagged = session.createCriteria(PostBean.class).list();
 
@@ -64,7 +74,6 @@ public class PostDAO {
     public PostBean getUnseenPost(UserBean user) {
 
         session.beginTransaction();
-        Criteria criteria;
 
         List<PostBean> choosable = session.createCriteria(PostBean.class).list();
         choosable = session.createCriteria(PostBean.class).add(Restrictions.eq("completed", 0)).list();
@@ -80,7 +89,6 @@ public class PostDAO {
 
     public PostBean getUnseenPostGendered(UserBean user, String gender) {
         session.beginTransaction();
-        Criteria criteria;
 
         List<PostBean> choosable = session.createCriteria(PostBean.class).list();
         choosable = session.createCriteria(PostBean.class).add(Restrictions.eq("completed", 0)).list();
@@ -131,6 +139,7 @@ public class PostDAO {
         post.getPoster().setScore(post.getPoster().getScore() + (likes.size() / 10));
         post.setCompleted(1);
         session.save(post);
+        session.getTransaction().commit();
 
     }
 
@@ -145,11 +154,151 @@ public class PostDAO {
         /* Creates new post */
         UserDAO userdao = new UserDAO();
         userdao.openConnection();
-        PostBean postBean = new PostBean(postModel.getImage(), postModel.getOccasion(), userdao.getUserByToken(postModel.getToken()));
+        Blob image = ImageConversionUtil.convertToBlob(postModel.getImage());
+        PostBean postBean = new PostBean(image, postModel.getOccasion(), userdao.getUserByToken(postModel.getToken()));
         userdao.closeConnection();
         session.beginTransaction();
         session.save(postBean);
-        session.getTransaction().commit();;
+        session.getTransaction().commit();
         return true;
+    }
+    
+	public void removePost(PostBean post) {
+		PostBean delPost=null;
+		Transaction tx=session.getTransaction();
+		try {
+			tx=session.beginTransaction();
+			delPost=(PostBean)session.get(PostBean.class, post);
+			if(delPost!=null) {
+				session.delete(delPost);
+				tx.commit();
+			}
+		}
+		catch(HibernateException e)
+		{
+			if(tx!=null)
+				tx.rollback();
+			e.printStackTrace();
+		}
+		finally {
+			session.close();
+		}	
+	}
+
+	public void like(RatingModel rating) {
+		PostBean post=null;
+		Transaction tx=session.getTransaction();
+		try {
+			tx=session.beginTransaction();
+			post=(PostBean)session.get(PostBean.class, rating.getPost());
+			if(post!=null) {
+				post.getLikes().add(new LikeBean(rating.getRater(), rating.getPost()));
+				if(!rating.getComment().equals(""))
+				{
+					post.getPost_comments().add(new CommentBean(rating.getPost(), rating.getRater(), rating.getComment()));
+				}
+				session.save(post);
+				tx.commit();
+			}
+		}
+		catch(HibernateException e)
+		{
+			if(tx!=null)
+				tx.rollback();
+			e.printStackTrace();
+		}
+		finally {
+			session.close();
+		}	
+	}
+	
+	public void dislike(RatingModel rating) {
+		PostBean post=null;
+		Transaction tx=session.getTransaction();
+		try {
+			tx=session.beginTransaction();
+			post=(PostBean)session.get(PostBean.class, rating.getPost());
+			if(post!=null) {
+				post.getDislikes().add(new DislikeBean(rating.getRater(), rating.getPost()));
+				if(!rating.getComment().equals(""))
+				{
+					post.getPost_comments().add(new CommentBean(rating.getPost(), rating.getRater(), rating.getComment()));
+				}
+				session.save(post);
+				tx.commit();
+			}
+		}
+		catch(HibernateException e)
+		{
+			if(tx!=null)
+				tx.rollback();
+			e.printStackTrace();
+		}
+		finally {
+			session.close();
+		}	
+	}
+
+    public boolean flagPost(FlagPostModel flagPostModel) {
+
+        int post_id = flagPostModel.getPostID();
+
+        /* Get post based on ID */
+        String hql = "From PostBean E WHERE E.post_id = :_id";
+        List posts
+                = session.createQuery(hql).setParameter("_id", post_id)
+                        .list();
+        if (posts.isEmpty()) {
+            return false;
+        } else {
+            session.beginTransaction();
+            PostBean postBean = (PostBean) posts.get(0);
+            postBean.setFlag(1);
+            session.update(postBean);
+            session.getTransaction().commit();
+            return true;
+        }
+
+    }
+
+    public boolean createAd(AdPostModel adPostModel) {
+
+        /* Creates new post */
+        UserDAO userdao = new UserDAO();
+        userdao.openConnection();
+        
+        /* Remove money from the sponsors balance based on amount to pay for ad */ 
+        UserBean sponsor = userdao.getUserByToken(adPostModel.getToken()); 
+      
+        /* create ad and save both the ad and the sponsor */
+        Blob image = ImageConversionUtil.convertToBlob(adPostModel.getImage());
+        AdvertisementBean adBean = new AdvertisementBean(image, adPostModel.getUrl(), sponsor);
+        
+        session.beginTransaction();
+        session.save(adBean);
+        session.getTransaction().commit();
+        userdao.closeConnection();
+        
+        this.saveNewBalance(adPostModel); 
+        
+        return true;
+    }
+    
+    /* Should make a check so user cannot go into negative balance */ 
+    private void saveNewBalance(AdPostModel adPostModel) {
+        
+        UserDAO userdao = new UserDAO();
+        userdao.openConnection();
+        
+        UserBean sponsor = userdao.getUserByToken(adPostModel.getToken()); 
+        int currrentBalance = sponsor.getBalance(); 
+        currrentBalance = currrentBalance - adPostModel.getAmountToPay(); 
+        sponsor.setBalance(currrentBalance);
+       
+        session.beginTransaction();
+        session.merge(sponsor); 
+        session.getTransaction().commit();
+        userdao.closeConnection();
+        
     }
 }
