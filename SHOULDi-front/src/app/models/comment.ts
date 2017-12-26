c78c6852f28ba2d16075df141b448f124238c5d9
Michael Warren <mwarren04011990@gmail.com>
2017-12-26 18:06:53 -0500

    made quick attempt at implementing comments section of feed.component.html

--- SHOULDi-front/src/app/models/comment.ts
+++ SHOULDi-front/src/app/models/comment.ts
@@ -1,7 +1,10 @@
+import { User } from "./user";
+
 export class Comment
 {
     _id          : number;
     _commenterId : number;
+    commenter    : User;
     content      : string;
     isFlagged    : boolean;
 }
