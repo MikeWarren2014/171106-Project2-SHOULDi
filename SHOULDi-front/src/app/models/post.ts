b1d76d05d3b124698224d1601254da220fc261a1
Jeffrey Myers <jeffm.ii@outlook.com>
2017-12-26 09:23:30 -0500

    Added occasion to post model

--- SHOULDi-front/src/app/models/post.ts
+++ SHOULDi-front/src/app/models/post.ts
@@ -3,6 +3,7 @@ export class Post {
     _id         : number;
     postDate    : Date;
     image       : Blob;
+    occasion    : string;
     comments    : Comment[]; 
     likes       : number;
     dislikes    : number;
