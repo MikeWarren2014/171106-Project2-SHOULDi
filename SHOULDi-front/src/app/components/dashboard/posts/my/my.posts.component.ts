6fff6193da90409012d5757868811f2e2c7cde8a
Jeffrey Myers <jeffm.ii@outlook.com>
2017-12-27 14:41:26 -0500

    updated my.posts

--- SHOULDi-front/src/app/components/dashboard/posts/my/my.posts.component.ts
+++ SHOULDi-front/src/app/components/dashboard/posts/my/my.posts.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
-
+import { Post } from "../../../../models/post";
+import { PostService } from "../../../../services/post.service";
 
 @Component({
     selector    : 'my-posts',
@@ -10,5 +11,22 @@ import { Component } from "@angular/core";
 })
 
 export class MyPostsComponent {
-    
+    posts : Post[];
+    message : any;
+    constructor(private postService : PostService){
+    }
+
+    ngOnInit() {
+        //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+        console.log(this.postService.getPostsFromUser(JSON.parse(localStorage.getItem("currentUser"))).subscribe(message => this.message = message));
+
+        //TODO: need to store posts to display 
+
+    }
+
+    ngOnDestroy() {
+        //Called once, before the instance is destroyed.
+        //Add 'implements OnDestroy' to the class.
+        
+    }
 }
