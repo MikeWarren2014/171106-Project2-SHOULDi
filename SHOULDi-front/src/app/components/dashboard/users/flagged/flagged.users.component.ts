b392974bf58398e4ad0736805709d9b5ac13cc39
Jeffrey Myers II <jeffm.ii@outlook.com>
2017-12-28 00:43:58 -0500

    Update flagged.users.component.ts
    deleting differences in our versions to resolve merge conflict

--- SHOULDi-front/src/app/components/dashboard/users/flagged/flagged.users.component.ts
+++ SHOULDi-front/src/app/components/dashboard/users/flagged/flagged.users.component.ts
@@ -2,7 +2,6 @@ import { Component } from "@angular/core";
 import { UserService } from "../../../../services/user.service";
 import { User } from "../../../../models/user";
 import { AutoUnsubscribe } from "../../../../autoUnsubscribe";
-import { Response } from "@angular/http/src/static_response";
 
 @Component({
     selector: "flagged-users",
@@ -12,13 +11,5 @@ import { Response } from "@angular/http/src/static_response";
 
 @AutoUnsubscribe
 export class FlaggedUsersComponent{
-    flaggedUsers : User[];
 
-    constructor(private userService : UserService)
-    {
-        // TODO: check this with the endpoint that Xavier,Caleb come up with     
-        userService.getFlaggedUsers().subscribe(users => {
-            this.flaggedUsers = users;
-        })
-    }
 }
